652595c8e62f364695e398d5f60c8312be67017c
Ash Simmonds <ashsimmonds@gmail.com>
2022-10-24 17:21:01 +1030

    testing rendering json object

--- src/Presentation.svelte
+++ src/Presentation.svelte
@@ -1,23 +1,38 @@
 <script>
-    import Title          from './slides/Title.svelte';
-    import Love           from './slides/Love.svelte';
-    import GettingStarted from './slides/GettingStarted.svelte';
-    import AutoAnimate from './slides/examples/AutoAnimate.svelte';
-    import Backgrounds from './slides/examples/Backgrounds.svelte';
-    import Barebones from './slides/examples/Barebones.svelte';
-    import LayoutHelpers from './slides/examples/LayoutHelpers.svelte';
-    import Markdown from './slides/examples/Markdown.svelte';
-    import Math from './slides/examples/Math.svelte';
-    import Media from './slides/examples/Media.svelte';
-    import Transitions from './slides/examples/Transitions.svelte';
+    import Title from "./slides/Title.svelte"
+    import Love from "./slides/Love.svelte"
+    import GettingStarted from "./slides/GettingStarted.svelte"
+    import AutoAnimate from "./slides/examples/AutoAnimate.svelte"
+    import Backgrounds from "./slides/examples/Backgrounds.svelte"
+    import Barebones from "./slides/examples/Barebones.svelte"
+    import LayoutHelpers from "./slides/examples/LayoutHelpers.svelte"
+    import Markdown from "./slides/examples/Markdown.svelte"
+    import Math from "./slides/examples/Math.svelte"
+    import Media from "./slides/examples/Media.svelte"
+    import Transitions from "./slides/examples/Transitions.svelte"
 
-    const partner = ['dude', 'sweet'];
+    import Final from "./slides/Final.svelte"
+
+    const partner = ["dude", "sweet"]
+
+    const someStuff = [
+        {
+            id: 1,
+            name: "some stuff",
+            description: "some stuff description",
+        },
+        {
+            id: 2,
+            name: "some other stuff",
+            description: "some other stuff description",
+        },
+    ]
 </script>
 
-<Title/>
-<Love {partner}/>
-<GettingStarted/>
-<AutoAnimate/>
+<Title />
+<Love {partner} />
+<!-- <GettingStarted/> -->
+<!-- <AutoAnimate/> -->
 <!-- <Backgrounds/> -->
 <!-- <Barebones/> -->
 <!-- <LayoutHelpers/> -->
@@ -26,3 +41,4 @@
 <!-- <Media/> -->
 <!-- <Transitions/> -->
 
+<Final {someStuff} />
---
+++ src/slides/Final.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let someStuff
+</script>
+
+<section>
+    <h2>Final page</h2>
+
+    <pre>
+        {#each someStuff as item}
+            <p>{JSON.stringify(item, null, 4)}</p>
+        {/each}
+    </pre>
+</section>
+
+<style>
+</style>
